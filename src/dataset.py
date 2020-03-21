11d1e6d45a81badb6c03afa9dd5dcfe7175feee1
Md. Masud Rana <masud.rana@infolytx.com>
2020-03-21 20:03:38 +0600

    add testdataset load

--- src/dataset.py
+++ src/dataset.py
@@ -64,3 +64,39 @@ class TrainDataset:
             'label': torch.tensor(self.labels[item], dtype = torch.long),
         }
 
+
+
+class TestDataset:
+    def __init__(self,img_height, img_width, mean, std):
+        df = pd.read_csv('../input/dataset/test.csv')
+        #df = df[['Image','label', 'kfold']]
+
+        #df = df[df.kfold.isin(folds)].reset_index(drop = True)
+
+        self.image_ids = df.Image.values
+
+        self.aug = albumentations.Compose([
+            albumentations.Resize(img_height, img_width),
+            albumentations.Normalize(mean, std, always_apply = True)
+        ])
+    
+
+
+    def __len__(self):
+        return len(self.image_ids)
+    
+    def __getitem__(self, item):
+        
+        image = Image.open(f'../input/dataset/Test Images/{self.image_ids[item]}')
+        image_id = self.image_ids[item]
+        #image = image.reshape(137,236).astype(float)
+        image = image.convert("RGB")
+        image = self.aug(image = np.array(image))["image"]
+
+        image = np.transpose(image, (2,0,1)).astype(np.float32)
+
+        return {
+            'image': torch.tensor(image, dtype = torch.float),
+            'image_id':image_id
+        }
+
